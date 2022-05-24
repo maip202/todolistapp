3962226416f685c64d4852dd1edd06c3b4a7e847
Miguel Iñiguez <maip202@gmail.comgit config --global user.name maip202>
2022-05-24 11:59:02 +0200

    todoListFirstPartDone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-todoListApp';
+  todos : Todo[] = [];
+  newTodo : string;
+
+  saveTodo(){
+    if(this.newTodo){
+        let todo = new Todo();
+        todo.name = this.newTodo;
+        todo.isCompleted = true;
+        this.todos.push(todo);
+        this.newTodo = "";
+    }else{
+      alert('Enter todo, please');
+    }
+  }
+
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  remove(id:number){
+    this.todos = this.todos.filter((v,i) => i !== id);
+  }
+
 }
